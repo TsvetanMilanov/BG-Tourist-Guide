624d02249cf0192a7a79619afd0d1f7911892b7c
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-01-30 20:38:17 +0200

    Initial commit.

---
+++ BG-Tourist-Guide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BG-Tourist-Guide
+//
+//  Created by Hakintosh on 1/30/16.
+//  Copyright © 2016 Hakintosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
